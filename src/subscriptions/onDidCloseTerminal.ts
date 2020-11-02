e29ae3c1f384ea0dc7e21026c8cac8879b7dd962
Ben Wildeman <wildeman.benjamin@gmail.com>
2020-11-02 13:17:25 +0000

    Update packages

--- src/subscriptions/onDidCloseTerminal.ts
+++ src/subscriptions/onDidCloseTerminal.ts
@@ -3,30 +3,38 @@ import common from "../common";
 
 export async function onDidCloseTerminal(closedTerminal: Terminal) {
     const closedTerminalID = await closedTerminal.processId;
-    const terminalExists = common.terminals.has(closedTerminalID);
 
-    if (!terminalExists) {
-        return;
-    }
+    if (closedTerminalID) {
+        const term = common.terminals.get(closedTerminalID);
 
-    common.terminalCount--;
-    let end: boolean = false;
-    const terminalIndex = Array.from(common.terminals.values()).findIndex(
-        (t) => t.terminalID === closedTerminalID,
-    );
-    common.terminals.get(closedTerminalID)?.terminal.dispose();
-    common.terminals.delete(closedTerminalID);
-
-    if (terminalIndex === common.terminalCount) {
-        end = true;
-    }
-
-    Array.from(common.terminals.values()).forEach(async ({ terminal }, i) => {
-        terminal.setTerminalIndex(i, terminal.name);
-
-        // Replicate the native VS Code showing of the next terminal when one is closed
-        if (common.loaded && i === (end ? terminalIndex - 1 : terminalIndex)) {
-            terminal.show();
+        if (!term) {
+            return;
         }
-    });
+
+        common.terminalCount--;
+        let end: boolean = false;
+        const terminalIndex = Array.from(common.terminals.values()).findIndex(
+            (t) => t.terminalID === closedTerminalID,
+        );
+        term.terminal.dispose();
+        common.terminals.delete(closedTerminalID);
+
+        if (terminalIndex === common.terminalCount) {
+            end = true;
+        }
+
+        Array.from(common.terminals.values()).forEach(
+            async ({ terminal }, i) => {
+                terminal.setTerminalIndex(i, terminal.name);
+
+                // Replicate the native VS Code showing of the next terminal when one is closed
+                if (
+                    common.loaded &&
+                    i === (end ? terminalIndex - 1 : terminalIndex)
+                ) {
+                    terminal.show();
+                }
+            },
+        );
+    }
 }
